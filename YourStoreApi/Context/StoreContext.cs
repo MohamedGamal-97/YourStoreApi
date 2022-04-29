dd7121c5f1bb7ce5e5c0d0e1dd6e1bfa74c3105f
mohamed gamal <midogemy100@gmail.com>
2022-04-29 05:58:12 +0200

    Initialize

---
+++ YourStoreApi/Context/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourStoreApi.Models;
+
+namespace YourStoreApi.Context
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ YourStoreApi/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace YourStoreApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
